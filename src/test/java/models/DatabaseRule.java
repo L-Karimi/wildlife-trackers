64d32524f203068bd3cd41823d9c5d9f7cd4788a
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-08 16:51:28 +0300

    override annotaion to assist in connecting to the database

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -10,7 +10,13 @@ public class DatabaseRule extends ExternalResource {
     protected void before() {
         DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "moringa", "lucy");
     }
-
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
 
         }
     }
