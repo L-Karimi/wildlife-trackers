e07008930851081ab61682746401e17692e1a82a
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-08 16:49:43 +0300

    applicate database rule

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -1,4 +1,18 @@
 package models;
 
-public class DatabaseRule {
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "moringa", "lucy");
+    }
+
+
+        }
+    }
+
 }
