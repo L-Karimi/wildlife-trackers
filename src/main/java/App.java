b654278549e30e4a18eb785c9a7bb9b68be51152
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-08 12:56:11 +0300

    route to direct to home page

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+
+import models.*;
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+public class App {
+    public static void main(String[] args){
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/newVulnerable", (req, res)->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "EndangeredAnimalForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
+    }
+
